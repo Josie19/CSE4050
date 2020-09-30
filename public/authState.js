aa9e6eeb46d394e4fdcd1ace13c7fdafca59665a
Jose Luiz Magallanes <006319894@coyote.csusb.edu>
2020-09-30 14:34:41 -0700

    Week 6 Progress

---
+++ public/authState.js
@@ -0,0 +1,23 @@
+var mainApp = {};
+//will check for change in User Key or ID and invoke automatic sign-out if true 
+(function(){
+    var firebase = app_fireBase;
+    var uid = null;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          uid = user.uid;
+        }
+        else
+        {
+            //redirect to login page
+            uid = null;
+            window.location.replace("./login.html");
+        }
+      });
+      function logOut(){
+        firebase.auth().signOut();
+      }
+      //creating an onclick event
+      mainApp.logOut = logOut;
+})()

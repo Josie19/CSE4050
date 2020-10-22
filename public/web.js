a955734c0eb677057007d4e12c67aaa6a23b0594
Jose Luiz Magallanes <006319894@coyote.csusb.edu>
2020-10-22 14:33:56 -0700

    Week 9 Progress

--- public/web.js
+++ public/web.js
@@ -13,5 +13,66 @@ var config = {
 
 firebase.initializeApp(config);
 app_firebase = firebase;
-//const firestore = firebase.firestore();
+var firestore = firebase.firestore();
+
+const docRef = firestore.doc("users/studentData");//syntax: users/<field_name> if error
+const outputHeader = document.querySelector("#outputText");
+const inputField = document.querySelector("#inputText");
+const saveButton = document.querySelector("#saveButton");
+const loaderButton = document.querySelector("#loadButton");
+
+saveButton.addEventListener("click", function(){
+    const textSave = inputField.value;
+    console.log("The input: "+textSave+", is now on firebase.");
+    docRef.set({
+        fullName: textSave
+    }).then(function(){
+        console.log("Input saved!");
+    }).catch(function(){
+         console.error("Error message: ", error);
+    })
+})
+
+loaderButton.addEventListener("click", function(){
+    docRef.get().then(function (doc) {
+        if(doc && doc.exists){
+            const myData = doc.data();
+            outputHeader.innerText = "Current in the cloud: "+ myData.fullName;
+        }
+    }).catch(function(error){
+        console.log("Error message", error);
+    })
+})
+
+getRealtimeUpdates = function(){
+    docRef.onSnapshot({includeMetadataChanges: true}, function (doc){
+        if(doc && doc.exitst){
+            const myData = doc.data();
+            console.log("Notification of incoming document ", doc);
+            outputHeader.innerText = "Name on Cloud: "+ myData.fullName;
+        }
+    })
+}
+
+getRealtimeUpdates();
+/*
+var dBase = firebase.firestore();//incorrect syntax?
+
+  dBase.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+.catch(function(error) {
+    console.error("Error adding document: ", error);//uncaught permissions
+});
+dBase.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
+*/
 })()
